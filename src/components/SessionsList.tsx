af84bd51351e2a686b163939db00a3a1c48b9b5e
Sahib <sahib.jaramillo@improving.com>
2022-08-08 17:18:44 -0500

    Improve SessionList visuals

--- src/components/SessionsList.tsx
+++ src/components/SessionsList.tsx
@@ -1,3 +1,9 @@
 export default function SessionsList() {
-  return <div>Sessions List</div>;
+  return (
+    <>
+      <div>(0) + laravel: 2 windows</div>
+      <div className="bg-lime-600">(1) + themux: 3 windows (attached)</div>
+      <div>(2) + typescript: 1 windows</div>
+    </>
+  );
 }
--- src/components/Terminal.tsx
+++ src/components/Terminal.tsx
@@ -7,10 +7,10 @@ export default function Terminal() {
   return (
     <div className="font-source-code-pro bg-slate-800 text-slate-50 rounded-xl">
       <div className="flex">
-        <Pane className="flex-grow border-b border-slate-900">
-          user@macbook-pro ~ $
+        <Pane className="basis-1/2 border-b border-slate-900">
+          sahib@macbook-pro ~ $
         </Pane>
-        <Pane className="flex-grow border-l border-b border border-lime-600">
+        <Pane className="basis-1/2 border-l border-b border-lime-600">
           <SessionsList />
         </Pane>
       </div>

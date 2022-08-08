d92ae6d184769d36f7e57c95c62c674bbdc7e8f8
Sahib <sahib.jaramillo@improving.com>
2022-08-08 17:18:22 -0500

    Enhance Clock component

--- src/components/Clock.tsx
+++ src/components/Clock.tsx
@@ -1,23 +1,29 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 import ClockColon from "./ClockColon";
 import ClockNumber from "./ClockNumber";
 
-type TimeType = {
+type TimeDigitsType = {
   hourFirstDigit: string;
   hourSecondDigit: string;
   minuteFirstDigit: string;
   minuteSecondDigit: string;
 };
 
-function getCurrentTime(): TimeType {
+function padZero([firstDigit, secondDigit]: string[]) {
+  return secondDigit === undefined
+    ? ["0", firstDigit]
+    : [firstDigit, secondDigit];
+}
+
+function getCurrentTimeDigits(): TimeDigitsType {
   const date = new Date();
-  const [hourFirstDigit, hourSecondDigit = "0"] = String(date.getHours()).split(
-    ""
+  const [hourFirstDigit, hourSecondDigit] = padZero(
+    date.getHours().toString().split("")
   );
-  const [minuteFirstDigit, minuteSecondDigit = "0"] = String(
-    date.getMinutes()
-  ).split("");
+  const [minuteFirstDigit, minuteSecondDigit] = padZero(
+    date.getMinutes().toString().split("")
+  );
 
   return {
     hourFirstDigit,
@@ -28,15 +34,14 @@ function getCurrentTime(): TimeType {
 }
 
 export default function Clock() {
-  const [time, setTime] = useState<TimeType>();
+  const [time, setTime] = useState<TimeDigitsType>(
+    useCallback(() => getCurrentTimeDigits(), [])
+  );
 
   useEffect(() => {
-    setInterval(() => setTime(getCurrentTime()), 1000);
+    const intervalId = setInterval(() => setTime(getCurrentTimeDigits()), 1000);
+    return () => clearInterval(intervalId);
   }, []);
-
-  if (time === undefined) {
-    return null;
-  }
 
   const {
     hourFirstDigit,
--- src/components/ClockNumber.tsx
+++ src/components/ClockNumber.tsx
@@ -1,90 +1,13 @@
+import numbers from "./clockNumbers";
+
 type PropsType = {
   number: number | string;
 };
 
-const X = true;
-const _ = false;
-
-// prettier-ignore
-const NUMBERS = [
-  [
-    X,X,X,X,X,
-    X,_,_,_,X,
-    X,_,_,_,X,
-    X,_,_,_,X,
-    X,X,X,X,X,
-  ],
-  [
-    _,_,_,_,X,
-    _,_,_,_,X,
-    _,_,_,_,X,
-    _,_,_,_,X,
-    _,_,_,_,X,
-  ],
-  [
-    X,X,X,X,X,
-    _,_,_,_,X,
-    X,X,X,X,X,
-    X,_,_,_,_,
-    X,X,X,X,X,
-  ],
-  [
-    X,X,X,X,X,
-    _,_,_,_,X,
-    X,X,X,X,X,
-    _,_,_,_,X,
-    X,X,X,X,X,
-  ],
-  [
-    X,_,_,_,X,
-    X,_,_,_,X,
-    X,X,X,X,X,
-    _,_,_,_,X,
-    _,_,_,_,X,
-  ],
-  [
-    X,X,X,X,X,
-    X,_,_,_,_,
-    X,X,X,X,X,
-    _,_,_,_,X,
-    X,X,X,X,X,
-  ],
-  [
-    X,X,X,X,X,
-    X,_,_,_,_,
-    X,X,X,X,X,
-    X,_,_,_,X,
-    X,X,X,X,X,
-  ],
-  [
-    X,X,X,X,X,
-    _,_,_,_,X,
-    _,_,_,_,X,
-    _,_,_,_,X,
-    _,_,_,_,X,
-  ],
-  [
-    X,X,X,X,X,
-    X,_,_,_,X,
-    X,X,X,X,X,
-    X,_,_,_,X,
-    X,X,X,X,X,
-  ],
-  [
-    X,X,X,X,X,
-    X,_,_,_,X,
-    X,X,X,X,X,
-    _,_,_,_,X,
-    _,_,_,_,X,
-  ],
-]
-
 export default function ClockNumber(props: PropsType) {
-  console.log(props.number);
-
   return (
     <div className="clock-number">
-      {NUMBERS[props.number].map((colorize, index) => (
+      {numbers[Number(props.number)].map((colorize: boolean, index: number) => (
         <div
           key={`cell-${index}`}
           className={colorize ? "bg-slate-200" : ""}

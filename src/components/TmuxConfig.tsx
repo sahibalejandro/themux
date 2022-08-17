a74f17f9434461bed2d1d0f15249f635b664857c
sahib <sahib.alejandro@gmail.com>
2022-08-17 00:48:49 -0500

    Ignore TmuxConfig component for now

--- src/components/TmuxConfig.tsx
+++ src/components/TmuxConfig.tsx
@@ -1,22 +1,23 @@
-import useStore from "../store";
+//import useStore from "../store";
 
 export default function TmuxConfig() {
-  const styles = useStore((state) => state.styles);
-  const config = `set-option -g status-style "fg=#${styles.status?.fg} bg=#${styles.status?.bg}"
-set-option -g status-left "#[fg=#${styles.statusLeft?.fg} bg=#${styles.statusLeft?.bg}] #S "
-set-option -g status-right ""
-set-option -g message-style "fg=#f3f2f7 bg=#3a3659"
-set-option -g message-command-style "fg=#b7b3d1 bg=#191716"
-set-option -wg window-status-separator ""
-set-option -wg window-status-format "#[fg=#b7b3d1] #{window_index} #{window_name} #{window_flags} "
-set-option -wg window-status-current-format "#[bg=#3a3659] #{window_index} #{window_name} #{window_flags} "
-set-option -wg mode-style "fg=#f3f2f7 bg=#3a3659"
-set-option -pg pane-border-style "fg=#191716"
-set-option -pg pane-active-border-style "fg=#a3be8c"`;
-
-  return (
-    <div className="max-w-screen-lg mx-auto">
-      <textarea className="w-full h-80 resize-none" value={config} readOnly />
-    </div>
-  );
+  return <div>not implemented yet</div>
+//  const styles = useStore((state) => state.styles);
+//  const config = `set-option -g status-style "fg=#${styles.status?.fg} bg=#${styles.status?.bg}"
+//set-option -g status-left "#[fg=#${styles.statusLeft?.fg} bg=#${styles.statusLeft?.bg}] #S "
+//set-option -g status-right ""
+//set-option -g message-style "fg=#f3f2f7 bg=#3a3659"
+//set-option -g message-command-style "fg=#b7b3d1 bg=#191716"
+//set-option -wg window-status-separator ""
+//set-option -wg window-status-format "#[fg=#b7b3d1] #{window_index} #{window_name} #{window_flags} "
+//set-option -wg window-status-current-format "#[bg=#3a3659] #{window_index} #{window_name} #{window_flags} "
+//set-option -wg mode-style "fg=#f3f2f7 bg=#3a3659"
+//set-option -pg pane-border-style "fg=#191716"
+//set-option -pg pane-active-border-style "fg=#a3be8c"`;
+//
+//  return (
+//    <div className="max-w-screen-lg mx-auto">
+//      <textarea className="w-full h-80 resize-none" value={config} readOnly />
+//    </div>
+//  );
 }
